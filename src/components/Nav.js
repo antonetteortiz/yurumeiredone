33f8ada13170a3e6fa190803f68b5276fff5b31e
Antonette_Ortiz <ORTIZANTONETTE@GMAIL.COM>
2021-12-30 19:52:51 -0500

    added dropdown element to navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,33 +1,76 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from "react";
+import React, { useRef, useState } from "react";
 import styled from "styled-components";
+import { FiChevronDown, FiChevronUp } from "react-icons/fi";
 import logo from "../img/logo/yurumeifavicon.svg";
 
 function Nav() {
+  const dropdownRef = useRef(null);
+  const [isActive, setIsActive] = useState(false);
+
+  const onClick = () => {
+    setIsActive(!isActive);
+  };
+
+  const subMenu = (
+    <ul
+      ref={dropdownRef}
+      className={`menu ${isActive ? "active" : "inactive"}`}
+    >
+      <li className="menu-item">
+        <a href="/food">FOOD</a>
+      </li>
+      <li className="menu-item">
+        <a href="/history">HISTORY</a>
+      </li>
+      <li className="menu-item">
+        <a href="/language">LANGUAGE</a>
+      </li>
+      <li className="menu-item">
+        <a href="/music">MUSIC</a>
+      </li>
+      <li className="menu-item">
+        <a href="/traditions">TRADITIONS</a>
+      </li>
+    </ul>
+  );
+
   return (
     <HeaderStyled>
-      <a className="logo" href="/">
-        <img src={logo} alt="" />
-
+      <div className="logo" href="/">
+        <a href="/">
+          <img src={logo} alt="" />
+        </a>
         <h1>
-        YURUMEI
+          <a href="/">YURUMEI</a>
         </h1>
-      </a>
+      </div>
+
       <ul className="nav-items">
-        <li>
-          <a href="/">HOME</a>
-        </li>
-        {/* <li>
-          <a href="#">ABOUT</a>
-        </li> */}
-        <li>
-          <a href="/faq">FAQs</a>
+        <li >
+          <a href="/" className="menu-item">
+            HOME
+          </a>
         </li>
         <li>
-          <a href="#">GARIFUNA CULTURE</a>
+          <a href="#" className="menu-item">
+            ABOUT
+          </a>
         </li>
         <li>
-          <a href="#">CONTACT US</a>
+          <a href="/faq" className="menu-item">
+            FAQs
+          </a>
+        </li>
+        <li onClick={onClick}>
+          GARIFUNA CULTURE
+          {isActive ? <FiChevronUp /> : <FiChevronDown />}
+          {isActive ? subMenu : null}
+        </li>
+        <li>
+          <a href="#contact" className="menu-item">
+            CONTACT US
+          </a>
         </li>
       </ul>
     </HeaderStyled>
@@ -35,35 +78,12 @@ function Nav() {
 }
 
 const HeaderStyled = styled.div`
-  min-height: 10vh;
-  margin: 0 20px;
+  height: 90px;
+  margin: 20px 20px;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  .nav-items {
-    display: flex;
-    align-items: center;
-    li {
-      margin: 0 1rem;
-      letter-spacing: 0.2rem;
-      padding: 0.2rem 0.6rem;
-      border-radius: 10px;
-      cursor: pointer;
-      transition: all 0.4s ease-in-out;
-      &:hover {
-        /* background-color: rgba(255, 204, 0, 1); */
-        text-decoration: underline;
-      }
-    }
-    .primary-btn {
-      margin-left: 3rem;
-      background-color: rgba(255, 204, 0, 0.6);
-    }
-  }
-
   .logo {
-    /* display: inline;
-    width: 250px; */
     letter-spacing: 0.5rem;
     margin-top: 5px;
     padding: 0.2rem 0.6rem;
@@ -100,37 +120,41 @@ const HeaderStyled = styled.div`
     h1 {
       float: right;
       padding-left: 5px;
-      :hover {
-        text-decoration: underline;
+    }
+  }
+  .nav-items {
+    display: inline-flex;
+    text-align: right;
+    /* position: relative; */
+    li {
+      margin-right: 0.8rem;
+      letter-spacing: 0.2rem;
+      padding: 0.2rem 0.6rem;
+      border-radius: 10px;
+      cursor: pointer;
+      transition: all 0.4s ease-in-out;
+      .menu-item {
+        &:hover {
+          text-decoration: underline;
+        }
       }
+    }
+    .menu {
+      visibility: hidden;
+    }
+    .menu.active {
+      opacity: .8;
+      visibility: visible;
+      transform: translateY(0);
+      text-decoration: none;
+      position: absolute;
+      text-align: left;
+      background-color: black;
+      color: white;
+      
     }
   }
 `;
 
 export default Nav;
 
-
-// .container {
-//     border-bottom: 2px solid #000000;
-//     display: flex;
-//     height: 200px;
-//     width: 100%;
-// }
-// .child {
-//     align-self: flex-end;
-//     width: 100px;
-//     height: 100px;
-//     background: #FFB6C1;
-//     animation-duration: 2s;
-//     animation-iteration-count: infinite;
-//     transform-origin: bottom;
-// }
-// .bounce:hover {
-//     animation-name: bounce;
-//     animation-timing-function: ease;
-// }
-// @keyframes bounce {
-//   0%   { transform: translateY(0); }
-//   50%  { transform: translateY(-100px); }
-//   100% { transform: translateY(0); }
-// }
